189705fdf47e7d34676f2d65d5b2e59904a5d66b
Anirudh Sridhar <as443@buffalo.edu>
2020-09-13 02:26:12 -0400

    minor link updates

--- front_end/src/intro.js
+++ front_end/src/intro.js
@@ -10,7 +10,7 @@ class Intro extends React.Component {
 	render() {
 		return <div className="text-center intro-text v-center">
 			<h1 className="light-blue intro-font">Welcome to <br /> Now This</h1>
-			<Link className="btn btn-outline-light" to="/student">For Students</Link>
+			<Link className="btn btn-outline-light" to="/classes">For Students</Link>
 			<Link className="btn btn-outline-light" to="/teacher" style={{marginLeft: "3.5rem"}}>For Teachers</Link>
 		</div>
 	}
